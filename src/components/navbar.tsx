983a30d8a5833768b94614701db161c3c0d843db
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-19 18:18:02 -0400

    Add initial navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const NavBar = () =>{
+
+    return(
+        <>
+            <nav className="bg-gray-950">
+                <div className="mx-auto max-w-7xl p-2 sm:px-8 lg:px-8">
+                    <div className="flex justify-center items-center">
+                        <Link href="/">
+                            <h2 className="text-white text-2xl">Rick and Morty</h2>
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
