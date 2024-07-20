78a537080edb22b16cdbf2fdd884d8f401f545d9
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-20 00:30:26 -0400

    Add GET and POST endpoints for all of users and install bcrypt library to hide the password

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,34 @@
+import {NextResponse} from "next/server";
+import {prisma} from "@/libs/prisma";
+import bcrypt from "bcrypt";
+
+export async function GET(request: Request) {
+    try {
+        const users = await prisma.user.findMany()
+        if (users.length !== 0) return NextResponse.json(users, {status: 200})
+        return NextResponse.json({"message": "There are no users!"}, {status: 404})
+    } catch (error) {
+        return NextResponse.json({error: "Internal Server Error"}, {status: 500})
+    }
+}
+
+export async function POST(request: Request) {
+    try {
+        const data = await request.json()
+        if(data.fullName == null) return NextResponse.json({"message": "full name was not found"}, {status: 404})
+        if(data.email == null) return NextResponse.json({"message": "Email was not found"}, {status: 404})
+        if(data.password == null) return NextResponse.json({"message": "Password was not found"}, {status: 404})
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(data.password, salt);
+        const newUser = await prisma.user.create({
+            data: {
+                fullName: data.fullName,
+                email: data.email,
+                password: hashedPassword
+            }
+        })
+        return NextResponse.json(newUser)
+    } catch (error) {
+        return NextResponse.json({error: "Internal Server Error"}, {status: 500})
+    }
+}
