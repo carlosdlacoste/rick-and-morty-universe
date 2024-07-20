50f218b57324b30f7ec1ecc5b5b31dce9299d593
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-20 18:36:55 -0400

    Adjust some bg colors to the sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,29 +1,40 @@
 "use client"
-import { Sidebar } from "flowbite-react";
+import { Sidebar, Flowbite } from "flowbite-react";
+import { twMerge } from 'tailwind-merge';
+
+export const customTheme = {
+    sidebar: {
+        root: {
+            base: twMerge('px-3 py-4', 'bg-one'), // Cambia esto al color que desees
+        },
+    },
+};
 
 
 const SideBar = () => {
     return (
-        <Sidebar aria-label="Sidebar with content separator example" className="bg-two !important">
-            <Sidebar.Items >
-                <Sidebar.ItemGroup>
-                    <Sidebar.Item href="#">
-                        Characters
-                    </Sidebar.Item>
-                    <Sidebar.Item href="#" >
-                        Episodes
-                    </Sidebar.Item>
-                </Sidebar.ItemGroup>
-                <Sidebar.ItemGroup>
-                    <Sidebar.Item href="/login" >
-                        Sign In
-                    </Sidebar.Item>
-                    <Sidebar.Item href="signup" >
-                        Sign Up
-                    </Sidebar.Item>
-                </Sidebar.ItemGroup>
-            </Sidebar.Items>
-        </Sidebar>
+        <Flowbite theme={{ theme: customTheme }}>
+            <Sidebar aria-label="Sidebar with content separator example">
+                <Sidebar.Items >
+                    <Sidebar.ItemGroup>
+                        <Sidebar.Item href="#" className="hover:bg-three">
+                            Characters
+                        </Sidebar.Item>
+                        <Sidebar.Item href="#" className="hover:bg-three">
+                            Episodes
+                        </Sidebar.Item>
+                    </Sidebar.ItemGroup>
+                    <Sidebar.ItemGroup>
+                        <Sidebar.Item href="/login" className="hover:bg-three">
+                            Sign In
+                        </Sidebar.Item>
+                        <Sidebar.Item href="signup" className="hover:bg-three">
+                            Sign Up
+                        </Sidebar.Item>
+                    </Sidebar.ItemGroup>
+                </Sidebar.Items>
+            </Sidebar>
+        </Flowbite>
     );
 }
 
