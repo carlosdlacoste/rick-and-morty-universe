79b52af8d40c196b957fa41d7cbe1619f560cb2e
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-19 22:57:58 -0400

    Add Rick and Morty's title logo in the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/logos/Rick_and_Morty.svg"
 
 const NavBar = () =>{
 
@@ -8,7 +10,7 @@ const NavBar = () =>{
                 <div className="mx-auto max-w-7xl p-2 sm:px-8 lg:px-8">
                     <div className="flex justify-center items-center">
                         <Link href="/">
-                            <h2 className="text-white text-2xl">Rick and Morty</h2>
+                            <Image src={logo} alt="Rick and Morty logo" width={150} height={100} />
                         </Link>
                     </div>
                 </div>
