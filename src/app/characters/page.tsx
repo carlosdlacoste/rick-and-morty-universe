216c5fbc83e5f51232ac3f465c6ba583c56d22fc
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-21 16:46:43 -0400

    Add container to characters table

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -19,32 +19,34 @@ const Characters = () =>{
     console.log(characters)
     return (
         <>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                    <TableHead className="w-[100px]">ID</TableHead>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Gender</TableHead>
-                    <TableHead>Species</TableHead>
-                    <TableHead>Status</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                        {
-                            characters.map(character => (
-                                <>
-                                    <TableRow>
-                                        <TableCell key={character.id}>{character.id}</TableCell>
-                                        <TableCell>{character.name}</TableCell>
-                                        <TableCell>{character.gender}</TableCell>
-                                        <TableCell>{character.species}</TableCell>
-                                        <TableCell>{character.status}</TableCell>
-                                    </TableRow>
-                                </>
-                            ))
-                        }
-                </TableBody>
-            </Table>
+            <div className="container mx-auto my-8">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                        <TableHead className="w-[100px]">ID</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Gender</TableHead>
+                        <TableHead>Species</TableHead>
+                        <TableHead>Status</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                            {
+                                characters.map(character => (
+                                    <>
+                                        <TableRow>
+                                            <TableCell key={character.id}>{character.id}</TableCell>
+                                            <TableCell>{character.name}</TableCell>
+                                            <TableCell>{character.gender}</TableCell>
+                                            <TableCell>{character.species}</TableCell>
+                                            <TableCell>{character.status}</TableCell>
+                                        </TableRow>
+                                    </>
+                                ))
+                            }
+                    </TableBody>
+                </Table>
+            </div>
         </>
     )
 }
