264a1c1588fc88867c638ccedf3d90afdc66cb11
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-21 15:49:49 -0400

    Install table component from shadcn library

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,6 +1,15 @@
 "use client"
 import { useEffect } from "react";
 import { useCharacterStore } from "@/store/characterStore";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+    } from "@/components/ui/table"
 
 const Characters = () =>{
     const { getCharacters, characters } = useCharacterStore();
