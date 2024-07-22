8f5aa898201afbcd5491b0b01353d1ff5e74bcab
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-22 00:34:20 -0400

    Add card to characterDetails component

--- src/app/characters/[id]/page.tsx
+++ src/app/characters/[id]/page.tsx
@@ -1,6 +1,15 @@
 "use client"
 import { useCharacterStore } from "@/store/characterStore"
 import { useRouter, useParams } from "next/navigation"
+import Image from "next/image"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 const CharacterDetails = () =>{
     const router = useRouter()
@@ -8,10 +17,31 @@ const CharacterDetails = () =>{
     const id = params.id
     const characters = useCharacterStore((state) => state.characters)
     const character = characters.find((char) => char.id === Number(id))
+    // console.log(character?.image)
 
     return(
         <>
-            <p className="text-center">Hola soy: {character?.name}</p>
+            <div  className="flex h-screen flex-col justify-center items-center px-6 py-12 lg:px-8">
+
+                <Card className="w-2/5 h-3/5 shadow-lg">
+                    <div className="flex flex-col justify-center items-center">
+                        <CardHeader>
+                            <CardTitle>{character?.name}</CardTitle>
+                        </CardHeader>
+                        <img src={character?.image} alt="Character icon" width={150} height={150} className="rounded-full"/>
+                        <CardContent>
+                            <div className="flex flex-col items-center py-4">
+
+                                <p className="text-sm">Gender: <span className="font-bold">{character?.gender}</span></p>
+                                <p className="text-sm">Species: <span className="font-bold">{character?.species}</span></p>
+                                <p className="text-sm">Status: <span className="font-bold">{character?.status}</span></p>
+                                <p className="text-sm">Type: <span className="font-bold">{character?.type == ''? 'classified': character?.type}</span></p>
+                            </div>
+
+                        </CardContent>
+                    </div>
+                </Card>
+            </div>
         </>
     )
 
