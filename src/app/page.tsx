ca0dbc67141b9d371028248af89942d576e8c4d0
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-22 11:50:37 -0400

    Add animation to home title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <div className="home-image">
-        <h3 className="text-center pt-7 text-white">Welcome to the Rick and Morty's Universe</h3>
+        <h3 className="text-center pt-7 text-white font-bold color-change-yellow">Welcome to the Rick and Morty's Universe</h3>
       </div>
     </>
   );
