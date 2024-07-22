cc82005c9452435de09ddb8c00fda18c2e1deb54
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-22 12:48:09 -0400

    Add new font-family to home page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <div className="home-image">
-        <h3 className="text-center pt-7 text-white font-bold color-change-yellow">Welcome to the Rick and Morty's Universe</h3>
+        <h1 className="text-4xl text-center pt-7 text-white font-bold color-change-yellow font-get-schwifty">Welcome to Rick and Morty's Universe</h1>
       </div>
     </>
   );
