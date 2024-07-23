92f9fa4a8880ced9493cd7bebf09c9288169d285
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-07-23 10:09:18 -0400

    Show episodes page only if the user is authenticated

--- src/app/episodes/page.tsx
+++ src/app/episodes/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect } from "react";
 import { useEpisodeStore } from "@/store/episodeStore";
+import { useAuthStore } from "@/store/authStore";
 import { Input } from "@/components/ui/input"
 import {
     Table,
@@ -14,6 +15,8 @@ import {
 
 const Episodes = () =>{
     const { getEpisodes, episodes, filter, setFilter } = useEpisodeStore();
+    const { token } = useAuthStore();
+
     useEffect(() => {
         getEpisodes()
     }, []);
@@ -29,42 +32,45 @@ const Episodes = () =>{
 
     return (
         <>
-            <div className="container mx-auto my-8">
-                <div className="flex justify-center items-center">
+            {
+                token &&
+                <div className="container mx-auto my-8">
+                    <div className="flex justify-center items-center">
 
-                    <Input
-                    type="text"
-                    value={filter}
-                    onChange={(event) => setFilter(event.target.value)}
-                    placeholder="Filter episodes..."
-                    className="mb-4 p-2 border border-gray-300 rounded w-1/3 h-1/3 ring-1 ring-inset ring-one focus:ring-2 focus:ring-inset focus:ring-one sm:text-sm sm:leading-6"
-                    />
+                        <Input
+                        type="text"
+                        value={filter}
+                        onChange={(event) => setFilter(event.target.value)}
+                        placeholder="Filter episodes..."
+                        className="mb-4 p-2 border border-gray-300 rounded w-1/3 h-1/3 ring-1 ring-inset ring-one focus:ring-2 focus:ring-inset focus:ring-one sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                    <Table className="bg-one rounded">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[100px] text-black font-bold">ID</TableHead>
+                                <TableHead className="text-black font-bold">Name</TableHead>
+                                <TableHead className="text-black font-bold">Air Date</TableHead>
+                                <TableHead className="text-black font-bold">Episode</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                                {
+                                    filteredEpisodes.map((episode, index) => (
+                                        <>
+                                            <TableRow  key={index} className="hover:bg-three cursor-pointer">
+                                                <TableCell>{episode.id}</TableCell>
+                                                <TableCell className="no-underline hover:underline hover:text-two">{episode.name}</TableCell>
+                                                <TableCell>{episode.air_date}</TableCell>
+                                                <TableCell>{episode.episode}</TableCell>
+                                            </TableRow>
+                                        </>
+                                    ))
+                                }
+                        </TableBody>
+                    </Table>
                 </div>
-                <Table className="bg-one rounded">
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="w-[100px] text-black font-bold">ID</TableHead>
-                            <TableHead className="text-black font-bold">Name</TableHead>
-                            <TableHead className="text-black font-bold">Air Date</TableHead>
-                            <TableHead className="text-black font-bold">Episode</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                            {
-                                filteredEpisodes.map((episode, index) => (
-                                    <>
-                                        <TableRow  key={index} className="hover:bg-three cursor-pointer">
-                                            <TableCell>{episode.id}</TableCell>
-                                            <TableCell className="no-underline hover:underline hover:text-two">{episode.name}</TableCell>
-                                            <TableCell>{episode.air_date}</TableCell>
-                                            <TableCell>{episode.episode}</TableCell>
-                                        </TableRow>
-                                    </>
-                                ))
-                            }
-                    </TableBody>
-                </Table>
-            </div>
+            }
         </>
     )
 }
